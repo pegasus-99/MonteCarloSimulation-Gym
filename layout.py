a60c657d2e3a6309a41af28f827711dd5bff3a9a
raoaaditya710 <raoaaditya710@gmail.com>
2022-11-27 17:09:27 -0600

    Added rao branch

--- layout.py
+++ layout.py
@@ -1,2 +1,19 @@
+class Machine:
+
+    def __init__(self, machineNumber, machineType):
+        self.machineNumber = machineNumber
+        self.machineType = machineType
+
+    def create_floor(self):
+        pass
+
+    def create_layout(self):
+        pass
+
+
+
+
+
+
 if __name__ == "__main__":
     pass
