80ed5d812209c60879e73c0d058c0a61217544b8
Shubham Jain <shubhamajain.27@gmail.com>
2022-11-27 16:58:04 -0600

    Added file Queues.py to work on queuing mechanism for the simulation

--- Queues.py
+++ Queues.py
@@ -1,2 +1,18 @@
+def make_queue(machine_name: object):
+    x=1
+    pass
+
+
+def add_user_to_queue(user: object, machine_name: object):
+    pass
+
+
+def check_queue(machines: object):
+    pass
+
+
 if __name__ == '__main__':
     pass
+
+
+
