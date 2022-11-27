6c7cb2226592661d3f700860fce75c099ffb41ca
pegasus-99 <meswani3@illinois.edu>
2022-11-27 17:10:26 -0600

    added functions for Users.py

--- Users.py
+++ Users.py
@@ -1,2 +1,21 @@
+class Users:
+
+    def __init__(self):
+        pass
+
+    def get_users(self):
+        pass
+
+    def set_users(self):
+        pass
+
+    def assign_properties(self):
+        # TODO: assign rigid/flexible
+        # TODO: assign impatience
+        pass
+
+    def add_to_queue(self):
+        pass
+
 if __name__ == "__main__":
     pass
