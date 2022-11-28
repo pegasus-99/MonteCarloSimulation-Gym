33b89a409a037aef86f73153b6e8d3c4400929a4
raoaaditya710 <raoaaditya710@gmail.com>
2022-11-27 18:25:21 -0600

    Updated function logic

--- layout.py
+++ layout.py
@@ -1,19 +1,27 @@
-class Machine:
+class Layout:
 
-    def __init__(self, machineNumber, machineType):
+    def __init__(self, totalFloors:int, totalMachines:int, machineNumber:float, machineType:str):
+        self.totalFloors = totalFloors
+        self.totalMachines = totalMachines
         self.machineNumber = machineNumber
         self.machineType = machineType
 
-    def create_floor(self):
+
+    def create_machine_space(self):
+        # Create ndarray depending upon floors
+        # Create 3x3 numpy array
+        # Add unique machine number at the centre
         pass
 
     def create_layout(self):
+        # Join machines floor-wise
+        # Create every possible layout depending on number of machines
         pass
 
-    def get_user(self):
+    def get(self):
         pass
 
-    def set_user(self):
+    def set(self):
         pass
 
 
