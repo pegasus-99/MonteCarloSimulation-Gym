5c6544cfa175a27b6c0c9ec6419c21d63f755b98
pegasus-99 <meswani3@illinois.edu>
2022-11-28 17:34:11 -0600

    get, set, assign_properties written

--- Users.py
+++ Users.py
@@ -1,22 +1,33 @@
+import random
 class Users:
 
-    def __init__(self, userName: str, userType: str, userPatience: str, timeDelta: float, viewPower: int):
+    def __init__(self, userName: str, userType: str, userPatience: str,
+                 timeDelta: float, viewPower: int, userID: str, userDict: dict):
         self.userName = userName
+        self.userType = userType
         self.userPatience = userPatience
         self.timeDelta = 0.0
         self.viewPower = 6
+        self.userID = userID
+        self.userDict = {}
 
-    def get(self):
-        return self.userName, self.userPatience, self.timeDelta, self.viewPower
+    def get(self, userDict, userType, userPatience):
+        userDict[self.userID] = [self.userName, userType, userPatience, self.viewPower, self.timeDelta]
+        return userDict
 
-    def set(self):
-        # TODO: update time delta
-        pass
+    def set(self, userDict, elapsedTime: float):
+        for key in userDict.keys():
+            userDict[key][-1] += elapsedTime
+        return userDict
 
-    def assign_properties(self):
+    def assign_properties(self, userType, userPatience):
         # TODO: assign rigid/flexible
         # TODO: assign impatience
-        pass
+        type = ['rigid', 'flexible']
+        pat = ['patient', 'impatient']
+        userType = random.choice(type)
+        userPatience = random.choice(pat)
+        return userType, userPatience
 
 
 if __name__ == "__main__":
