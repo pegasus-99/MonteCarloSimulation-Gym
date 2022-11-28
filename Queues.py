fd77826c6111876b21b7eb90ada361cf014af133
Shubham Jain <shubhamajain.27@gmail.com>
2022-11-28 12:51:48 -0600

    added workflow comments

--- Queues.py
+++ Queues.py
@@ -1,17 +1,20 @@
 def make_queue(machine_name: object):
+    # initialize queue for the machine
     pass
 
 
 def add_user_to_queue(user: object, machine_name: object):
+    # initialize user to the machine queue
+    # update User object with current_queue
+    # update machine object with user
     pass
 
 
 def check_queue(machines: object):
+    # check for impatient user
+    # check if machine is empty
     pass
 
 
 if __name__ == '__main__':
     pass
-
-
-
