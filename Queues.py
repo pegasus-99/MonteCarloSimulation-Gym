dfdbf117d2a667fe67b413dd83604bdea52fd270
Shubham Jain <shubhamajain.27@gmail.com>
2022-11-28 17:29:12 -0600

    initial function code for check_queue, make_queue, and add_user_to_queue with pseudo code for staging area -- integration level code

--- Queues.py
+++ Queues.py
@@ -1,19 +1,41 @@
-def make_queue(machine_name: object):
+# for staging area, import Layout
+import time
+
+
+def make_queue(machine: object):
     # initialize queue for the machine
-    pass
+    machine.queue = [None] * 5
+    return machine
 
 
-def add_user_to_queue(user: object, machine_name: object):
+def add_user_to_queue(user: object, machine: object):
     # initialize user to the machine queue
     # update User object with current_queue
     # update machine object with user
-    pass
+
+    # make the user scan here for a new machine with a smaller queue -- Layout.find_new_machine()
+    if None not in machine.queue:
+        return False
+    # updating machine queue
+    machine.queue.append(user)
+    # updating users' current queue
+    setattr(user, "current_queue", machine.queue)
+    return True
 
 
-def check_queue(machines: object):
+def check_queue(machineList: object):
+    # run this piece of code in a thread
     # check for impatient user
     # check if machine is empty
-    pass
+    time.sleep(5)
+    for machine in machineList:
+        for user in machine:
+            if user.impatience:
+                if user.currentQueueTime > user.impatientTime or user.currentQueueTime > user.thresholdTime:
+                    # find new machine using Layout --->  Layout.find_new_machine(user, user.currentMachine)
+                    pass
+
+
 
 
 if __name__ == '__main__':
