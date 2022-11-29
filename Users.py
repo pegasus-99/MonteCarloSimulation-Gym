4deadd21f1fd6698d2c0ca4d7875e980d29550e4
pegasus-99 <meswani3@illinois.edu>
2022-11-28 18:05:18 -0600

    edited set and assign properties

--- Users.py
+++ Users.py
@@ -7,7 +7,7 @@ class Users:
         :param userName: first name of the user
         :param userType: rigid or flexible
         :param userPatience: patient or impatient
-        :param timeDelta: initializes time spent by user in the gym
+        :param timeDelta: time spent by user in the gym
         :param viewPower: the field of view of each user
         :param userID: Unique ID assigned to each user
         :param userDict: initializes a dictionary of users
@@ -15,20 +15,22 @@ class Users:
         self.userName = userName
         self.userType = userType
         self.userPatience = userPatience
-        self.timeDelta = 0.0
-        self.viewPower = 6
+        self.timeDelta = timeDelta
+        self.viewPower = viewPower
         self.userID = userID
-        self.userDict = {}
+        self.userDict = userDict
 
-    def get(self, userDict, userType, userPatience):
+    def get(self, userDict, userType, userPatience, timeDelta, viewPower):
         """
         This method gets information about the user
-        :param userDict: the dictionary of all users and their initialized properties
+        :param viewPower: The field of view of the user initialized from assign_properties()
+        :param timeDelta: the time spent working out initialized from assign_properties()
+        :param userDict: the dictionary of all users and their initialized properties from assign_properties()
         :param userType: type of user randomly assigned from assign_properties()
         :param userPatience: user patience randomly assigned from assign_properties()
         :return: current user's dictionary
         """
-        userDict[self.userID] = [self.userName, userType, userPatience, self.viewPower, self.timeDelta]
+        userDict[self.userID] = [self.userName, userType, userPatience, viewPower, timeDelta]
         return userDict
 
     def set(self, elapsedTime: float):
@@ -45,13 +47,17 @@ class Users:
     def assign_properties(self):
         """
         Assign user properties randomly
-        :return: the randomly selected type and patience
+        :return: the randomly selected type and patience and assigned viewPower and initialize time delta to 0
+                 and the initialized user dictionary
         """
         ironheadType = ['rigid', 'flexible']
         ironheadPat = ['patient', 'impatient']
         self.userType = random.choice(ironheadType)
         self.userPatience = random.choice(ironheadPat)
-        return self.userType, self.userPatience
+        self.timeDelta = 0
+        self.viewPower = 6
+        self.userDict = {}
+        return self.userType, self.userPatience, self.timeDelta, self.viewPower, self.userDict
 
 
 if __name__ == "__main__":
