bce684cadaf5755845ec85bfd38e7ce05d5e0375
pegasus-99 <meswani3@illinois.edu>
2022-12-01 15:44:57 -0600

    initialized Machine variables

--- machine.py
+++ machine.py
@@ -1,12 +1,11 @@
 class Machine:
-    def __int__(self):
-        pass
+    def __init__(self, totalMachines, useTime, peopleCount, machineType):
+        self.totalMachines = totalMachines
+        self.useTime = useTime
+        self.peopleCount = peopleCount
+        self.machineType = machineType
         """
-        :type
-        time to use
         current_queue: [user1, user2....] --> object queue
-        count of people who have used it
-        
         """
 
 
