298f286ec3363abaef47aa54875b2a2ac9d6b620
pegasus-99 <meswani3@illinois.edu>
2022-12-01 15:47:04 -0600

    get and set for Machine

--- machine.py
+++ machine.py
@@ -7,5 +7,10 @@ class Machine:
         """
         current_queue: [user1, user2....] --> object queue
         """
+    def get(self):
+        return self.totalMachines, self.useTime, self.peopleCount, self.machineType
+
+    def set(self):
+        pass
 
 
