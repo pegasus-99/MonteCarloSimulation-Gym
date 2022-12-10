dee79f1e502b728c0899cb503792b6ec948da282
raoaaditya710 <raoaaditya710@gmail.com>
2022-12-10 16:20:52 -0600

    Updated code: integrated Machine module

--- layout.py
+++ layout.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 import numpy as np
+import machine
 
 
 def find_new_machine(fieldOfView: int, layout: np.ndarray, currentUserMachine: object):
@@ -10,6 +11,7 @@ def find_new_machine(fieldOfView: int, layout: np.ndarray, currentUserMachine: o
     :return:
     """
 
+    # Find index position of current machine ID in layout
     currentMachinePosition = np.where(layout == currentUserMachine.machineID)
 
     nearbyMachines = []
@@ -28,12 +30,12 @@ def find_new_machine(fieldOfView: int, layout: np.ndarray, currentUserMachine: o
                     if row >= 0 and col >= 0 and not np.isnan(layout[row][col]) \
                             and layout[row][col] != currentUserMachine.machineID:
 
-                        machine = layout[row][col]
+                        machineNum = layout[row][col]
 
                         # Call function to get machine queue length
                         # Add to dictionary as {mach: queueLength}
                         # Return machine with least queueLength
-                        nearbyMachines.append(machine)
+                        nearbyMachines.append(machineNum)
 
                 except IndexError:
                     pass
@@ -55,15 +57,18 @@ class Layout:
         :param totalMachines:
         """
         # self.totalFloors = totalFloors
-        self.totalMachines = totalMachines
+        # self.allMachines = allMachines
+        # self.totalMachines = totalMachines
         self.totalFloors = totalFloors
 
-    def create_machine_space(self):
+    def create_machine_space(self, allMachines: object):
         """
         :return: number of rows:int, number of columns:int, nested list containing unique machine numbers
         """
 
-        totalMachines = self.totalMachines
+        # Get total machines from Machine  module? Data Structure = List
+        # totalMachines = self.totalMachines
+        totalMachines = len(allMachines)
         totalFloors = self.totalFloors
 
         floorLayout = {}
@@ -73,27 +78,31 @@ class Layout:
             machinesOnFloor = round(totalMachines / totalFloors)
 
             # Dictionary where key = floor number, val = list containing machine numbers
+            # Appending current floor number as key
             if floor + 1 not in floorLayout:
                 floorLayout[floor + 1] = []
 
             # Get total number of machines in previous floor
             try:
-                lastMachine = floorLayout[floor][-1]
+                lastMachine = floorLayout[floor][-1].machineID
             except KeyError:
                 pass
 
             # Loop over data structure given from Bum
-            # [[machineID = mach+1, 10, ], [m2], [m3]]
-            for mach in range(totalMachines):
-                if mach + 1 <= machinesOnFloor:
+            # allMachines = [[machineID = mach+1, 10, ], [m2], [m3]]
+            # for mach in range(totalMachines):
+            for mach in allMachines:
+                if mach.machineID <= machinesOnFloor:
                     # currentMachine = machine.Machine(machineID = mach+1, useTime= , )
 
                     # Add list of machines to dictionary
                     if floor + 1 == 1:
-                        floorLayout[floor + 1].append(mach + 1)
+                        # floorLayout[floor + 1].append(mach + 1)
+                        floorLayout[floor + 1].append(mach)
                     else:
-                        floorLayout[floor + 1].append(lastMachine + (mach + 1))
-                        # floorLayout[floor + 1].append(lastMachine.MachineID + (mach+1))
+                        # floorLayout[floor + 1].append(lastMachine + (mach + 1))
+                        if mach.machineID > lastMachine:
+                            floorLayout[floor + 1].append(mach)
 
             totalMachines = totalMachines - machinesOnFloor
             totalFloors -= 1
@@ -105,7 +114,6 @@ class Layout:
 
             if floor not in layoutCombination:
                 layoutCombination[floor] = machineArrangement
-                # layoutCombination[floor].append(machineArrangement)
 
         return layoutCombination
 
@@ -147,7 +155,7 @@ class Layout:
                     for col in range(1, machineArea.shape[1], 3):
                         # Adding values to array by iterating over layout (list index value)
                         try:
-                            machineArea[row, col] = machines[index]
+                            machineArea[row, col] = machines[index].machineID
                             index += 1
                         except IndexError:
                             pass
